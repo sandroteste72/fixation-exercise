911320304addb9504de4495d4a37a930447c1441
Sandro Arpi <sandro.teste72@gmail.com>
2020-09-08 23:07:48 -0300

    Adicionando classes

---
+++ FixationExercise/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixationExercise.Entities
+{
+    class Order
+    {
+    }
+}
---
+++ FixationExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FixationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
